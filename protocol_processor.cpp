911fb5dd9d79ba9cdbc4556167471d3a206e2d5f
Francisco Bertolotto <fbertolotto@fi.uba.ar>
2020-11-05 11:04:08 -0300

    Elimino protocolo

--- protocol_processor.cpp
+++ protocol_processor.cpp
@@ -5,33 +5,58 @@
 #include <vector>
 
 void ProtocolProcessor::process(std::string msg) {
-  std::vector<std::string> lines = protocol.get_lines(msg);
+  std::vector<std::string> lines = get_lines(msg);
   start(lines);
 }
 
 void ProtocolProcessor::start(std::vector<std::string> lines) {
   bool body = false;
-  protocol.set_command(lines[0]);
+  set_command(lines[0]);
   for (size_t i = 1; i < lines.size(); i++) {
     std::string line = lines[i];
     if (body)
-      protocol.set_body(line);
+      set_body(line);
     else if (!line.length())
       body = true;
   }
 }
 
 void ProtocolProcessor::show_all() {
-  std::string first_line = protocol.get_method() + " " +
-                           protocol.get_resource() + " " +
-                           protocol.get_protocol() + "\n";
+  std::string first_line =
+      get_method() + " " + get_resource() + " " + get_protocol() + "\n";
 
   std::cout << first_line;
-  std::cout << protocol.get_body() << "\n";
+  std::cout << get_body() << "\n";
 }
 
-std::string ProtocolProcessor::get_method() { return protocol.get_method(); }
-std::string ProtocolProcessor::get_body() { return protocol.get_body(); }
-std::string ProtocolProcessor::get_resource() {
-  return protocol.get_resource();
+std::string ProtocolProcessor::get_method() { return commands[0]; }
+std::string ProtocolProcessor::get_resource() { return commands[1]; }
+std::string ProtocolProcessor::get_protocol() { return commands[2]; }
+std::string ProtocolProcessor::get_body() { return body; }
+
+std::vector<std::string> ProtocolProcessor::split(std::string msg,
+                                                  std::string delimiter,
+                                                  bool ignore_empty) {
+  size_t init = 0, delim_pos;
+  std::vector<std::string> buffer;
+  while ((delim_pos = msg.find(delimiter, init)) != std::string::npos) {
+    std::string word = msg.substr(init, delim_pos - init);
+    init = delim_pos + 1;
+    if (word.length() == 0 and ignore_empty) continue;
+    buffer.push_back(word);
+  }
+  buffer.push_back(msg.substr(init));
+  return buffer;
+}
+
+void ProtocolProcessor::set_command(std::string command) {
+  commands = split(command, " ", true);
+}
+
+void ProtocolProcessor::set_body(std::string body) {
+  this->body += body + "\n";
+}
+
+std::vector<std::string> ProtocolProcessor::get_lines(std::string msg) {
+  return split(msg, "\n", false);
 }
--- protocol_processor.h
+++ protocol_processor.h
@@ -1,16 +1,22 @@
 #include <string>
-
-#include "protocol.h"
+#include <vector>
 
 class ProtocolProcessor {
  private:
-  Protocol protocol;
+  std::vector<std::string> commands;
+  std::string body;
+  std::vector<std::string> split(std::string msg, std::string delimiter,
+                                 bool ignore_empty);
   void start(std::vector<std::string> lines);
+  void set_command(std::string command);
+  void set_body(std::string body);
+  std::vector<std::string> get_lines(std::string msg);
 
  public:
   void process(std::string msg);
   void show_all();
   std::string get_method();
-  std::string get_body();
   std::string get_resource();
+  std::string get_protocol();
+  std::string get_body();
 };
