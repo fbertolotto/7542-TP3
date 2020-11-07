7c194685df7d7cc0672eb1e27cb741a1564d7e44
Francisco Bertolotto <fbertolotto@fi.uba.ar>
2020-11-07 13:25:03 -0300

    Client lee de STDIN

--- client_src/client_main.cpp
+++ client_src/client_main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include "../common_src/socket.h"
 int main(int argc, char** argv) {
   Socket client("localhost", "8080");
   client.connect_to_sv();
-  std::string c1 = argv[1];
-  std::string msg = c1 + " /root HTTP1.1\n\nHOLA!";
-  client.send_msg(msg, msg.size());
+  std::stringstream text;
+  std::string stdin_buf;
+  while (getline(std::cin, stdin_buf)) {
+    text << stdin_buf << "\n";
+  }
+
+  std::string order = text.str();
+  client.send_msg(order, order.size());
+
   client.stop_sending();
   std::string buffer;
   client.recv_msg(buffer);
